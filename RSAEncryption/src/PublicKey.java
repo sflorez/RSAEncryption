e5c2ea4eafe9c7d45571cafa2aa7bf015cfeae46
Jacob Koko <jykoko@stetson.edu>
2014-04-21 22:02:17 -0400

    Update PublicKey.java
    Another quick fix, we forgot to add public to our accessor methods.

--- RSAEncryption/src/PublicKey.java
+++ RSAEncryption/src/PublicKey.java
@@ -6,27 +6,27 @@ public class PublicKey
 	 
 	 public PublicKey(BigInteger n, BigInteger e)
 	 {
-		 this.e = e;
-		 this.n = n;
+	     this.e = e;
+	     this.n = n;
 	 }
 	 
 	 public void setN(BigInteger myN)
 	 {
-		  n = myN;
+       	      n = myN;
 	 }
 	  
-	  public void setE(BigInteger myE)
-	  {
-		  e = myE;
-	  }
+	 public void setE(BigInteger myE)
+	 {
+	      e = myE;
+	 }
 	  
-	  BigInteger getE()
-	  {
-		 return e; 
-	  }
+	 public BigInteger getE()
+	 {
+	      return e; 
+	 }
 	  
-	  BigInteger getN()
-	  {
-		  return n;
-	  }
+	 public BigInteger getN()
+	 {
+	      return n;
+	 }
 }
